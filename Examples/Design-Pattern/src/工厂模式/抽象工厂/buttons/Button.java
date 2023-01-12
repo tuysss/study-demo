dd114c1c325cec3a4b7d835396b30f1454d70acf
tianyanning <tianyanning@kuaishou.com>
2023-01-12 16:03:54 +0800

    add mini-spring-study and other modify

---
+++ Examples/Design-Pattern/src/工厂模式/抽象工厂/buttons/Button.java
@@ -0,0 +1,10 @@
+package 工厂模式.抽象工厂.buttons;
+
+/**
+ * @author tuysss
+ * @date 2022/12/13
+ * @description https://refactoringguru.cn/design-patterns/abstract-factory/java/example
+ */
+public interface Button {
+     void print();
+}
