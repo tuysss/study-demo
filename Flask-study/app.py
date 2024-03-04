af78fd79d8ac1d8f5b595c6a4d6bbfc3059b9509
tuysss <2862670249@qq.com>
2024-03-04 17:42:10 +0800

    Add index page

--- Flask-study/app.py
+++ Flask-study/app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/home')  # decorator, bond the function to the corresponding url
-@app.route('/')
 @app.route('/index')
 def hello():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
@@ -13,3 +12,22 @@ def hello():
 @app.route('/user/<name>')
 def user_page(name):
     return f'User: {escape(name)}'
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
